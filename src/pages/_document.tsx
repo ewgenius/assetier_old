2d4d33733d519d030cda1a0970bcd14c3379dac5
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-10 22:49:23 +0300

    init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="Assetier" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
