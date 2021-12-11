2d87e8a49908fdc554ee7ee8f4214c7e7326ed49
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-11 23:13:38 +0300

    fix

--- src/utils/octokit.ts
+++
@@ -1,6 +0,0 @@
-import { Octokit } from "octokit";
-
-// const app = new App({
-//   appId: process.env.GITHUB_APP_ID as string,
-//   privateKey: process.env.GITHUB_APP_PRIVATE_KEY as string,
-// });
