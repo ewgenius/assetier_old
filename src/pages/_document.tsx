f120603b276823bbf49267d6ce9c9a95228b748f
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-11 19:39:38 +0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,13 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className="h-full">
         <Head>
           <meta name="description" content="Assetier" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
-        <body>
+        <body className="h-full">
           <Main />
           <NextScript />
         </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
-const Home: NextPage = () => {
-  return (
-    <div className="min-h-screen w-screen flex flex-col">
-      <Head>
-        <title>Assetier | App</title>
-      </Head>
-      <main className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="font-semibold text-4xl">📦 app</h1>
-        <p className="text-gray-400 mt-2">coming soon...</p>
-      </main>
-      <footer className="flex flex-col items-center p-2 text-gray-400 text-sm">
-        <p>&copy; 2021 assetier.app</p>
-      </footer>
-    </div>
-  );
+export const Home: NextPage = () => {
+  return <div>App</div>;
 };
 
 export default Home;
