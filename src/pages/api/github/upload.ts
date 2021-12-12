316b8e3e200b66aaeb34f2fc42ab2e2eeb7f4eb1
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-12 18:04:08 +0300

    support upload

--- src/pages/api/github/upload.ts
+++ src/pages/api/github/upload.ts
@@ -1,8 +1,18 @@
+import * as fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { App } from "@octokit/app";
 import { getSession } from "next-auth/react";
+import formidable, { Files, Fields, File as FFile } from "formidable";
 import { getGitHubPrivateKey } from "@utils/getGitHubPrivateKey";
+import PersistentFile from "formidable/PersistentFile";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -27,54 +37,94 @@ export default async function handler(
     Number(req.query.installation_id)
   );
 
-  const body = JSON.parse(req.body);
+  const form = formidable({});
+  const { body, files } = await new Promise<{ body: Fields; files: Files }>(
+    (resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve({
+          body: fields,
+          files,
+        });
+      });
+    }
+  );
 
-  // TODO: specify default branch in the UI
   const branches = await octokit.request("GET /repos/{owner}/{repo}/branches", {
-    owner: body.owner,
-    repo: body.repository,
+    owner: body.owner as string,
+    repo: body.repository as string,
   });
 
   const sha = branches.data[0].commit.sha;
-
-  console.log(sha);
-
   const branchName = `assetier/${uuidv4()}`;
-  const newBranch = await octokit.request(
-    "POST /repos/{owner}/{repo}/git/refs",
-    {
-      owner: body.owner,
-      repo: body.repository,
-      ref: `refs/heads/${branchName}`,
-      sha,
+  await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
+    owner: body.owner as string,
+    repo: body.repository as string,
+    ref: `refs/heads/${branchName}`,
+    sha,
+  });
+
+  if (files) {
+    const dataToUpload = await Promise.all(
+      Object.values(files).map(
+        (file) =>
+          new Promise<{ file: FFile; buffer: Buffer }>((resolve, reject) =>
+            fs.readFile((file as any as FFile).filepath, (err, buffer) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve({ file: file as any as FFile, buffer });
+              }
+            })
+          )
+        // .then(({ file, buffer }) => {
+        //   console.log(`uploading ${file.originalFilename}`);
+        //   return octokit
+        //     .request("PUT /repos/{owner}/{repo}/contents/{path}", {
+        //       owner: body.owner as string,
+        //       repo: body.repository as string,
+        //       path: (body.path as string) + "/" + file.originalFilename,
+        //       message: `update from Assetier: ${file.originalFilename}`,
+        //       content: buffer.toString("base64"),
+        //       branch: branchName,
+        //     })
+        //     .then(() => {
+        //       console.log(`uploaded ${file.originalFilename}`);
+        //     })
+        //     .catch((e) => {
+        //       console.log(`failed to upload ${file.originalFilename}\n${e}`);
+        //     });
+        // })
+      )
+    );
+
+    for (let { buffer, file } of dataToUpload) {
+      await octokit
+        .request("PUT /repos/{owner}/{repo}/contents/{path}", {
+          owner: body.owner as string,
+          repo: body.repository as string,
+          path: (body.path as string) + "/" + file.originalFilename,
+          message: `update from Assetier: ${file.originalFilename}`,
+          content: buffer.toString("base64"),
+          branch: branchName,
+        })
+        .then(() => {
+          console.log(`uploaded ${file.originalFilename}`);
+        });
     }
-  );
+  }
 
-  console.log(newBranch.data);
-
-  const newFile = await octokit.request(
-    "PUT /repos/{owner}/{repo}/contents/{path}",
-    {
-      owner: body.owner,
-      repo: body.repository,
-      path: body.path,
-      message: "update from Assetier",
-      content: Buffer.from(body.content).toString("base64"),
-      branch: branchName,
-    }
-  );
-
-  console.log(newFile.data);
+  console.log("all files uploaded");
 
   const pr = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
-    owner: body.owner,
-    repo: body.repository,
+    owner: body.owner as string,
+    repo: body.repository as string,
     title: "Assetier update",
     base: "main",
     head: branchName,
   });
-
-  console.log(pr.data);
 
   res.status(200).json({ name: "John Doe" });
 }
--- src/pages/setup/repository.tsx
+++ src/pages/setup/repository.tsx
@@ -1,11 +1,11 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import type { NextPage, GetServerSideProps } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { App } from "@octokit/app";
 import { AuthBlock } from "@components/AuthBlock";
 import { getGitHubPrivateKey } from "@utils/getGitHubPrivateKey";
-
+import { FolderIcon, SearchIcon, UploadIcon } from "@heroicons/react/outline";
 export interface SetupProps {
   icons: {
     name: string | null;
@@ -16,6 +16,113 @@ export interface SetupProps {
     };
   }[];
 }
+
+export const PathInput = () => {
+  const { query } = useRouter();
+
+  const [path, setPath] = useState<string>(query.path as string);
+
+  return (
+    <div className="my-2">
+      <label htmlFor="path" className="block text-sm font-medium text-gray-700">
+        Search candidates
+      </label>
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <div className="relative flex items-stretch flex-grow focus-within:z-10">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <FolderIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
+          <input
+            type="text"
+            name="path"
+            id="path"
+            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border border-gray-300"
+            placeholder="path to folder with svg files"
+            value={path}
+            onChange={({ target }) => setPath(target.value)}
+          />
+        </div>
+        <Link
+          href={`/setup/repository?installation_id=${query.installation_id}&owner=${query.owner}&repository=${query.repository}&path=${path}`}
+        >
+          <a className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export const Upload = () => {
+  const { query } = useRouter();
+
+  const [files, setFiles] = useState<File[]>([]);
+
+  const upload = useCallback(() => {
+    const data = new FormData();
+    files.forEach((file) => {
+      data.set(file.name, file);
+    });
+
+    data.set("owner", query.owner as string);
+    data.set("repository", query.repository as string);
+    data.set("path", query.path as string);
+
+    fetch(`/api/github/upload?installation_id=${query.installation_id}`, {
+      method: "POST",
+      body: data,
+      // JSON.stringify({
+      //   owner: query.owner,
+      //   repository: query.repository,
+      //   path: query.path + "/" + name,
+      // }),
+    }).then(() => {
+      setFiles([]);
+    });
+  }, [files]);
+
+  return (
+    <label className="my-2 cursor-pointer">
+      <div
+        // htmlFor="files"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Upload
+      </div>
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <div className="relative flex items-stretch flex-grow focus-within:z-10">
+          <div className="focus:ring-indigo-500 focus:border-indigo-500 w-full rounded-none rounded-l-md pl-2 pt-2 text-gray-500 sm:text-sm border border-gray-300 flex flex-row">
+            {files && files.length > 0
+              ? files.map((file) => (
+                  <img
+                    className="w-5 h-5 mr-1"
+                    src={URL.createObjectURL(file)}
+                  />
+                ))
+              : "Upload files..."}
+          </div>
+          <input
+            multiple
+            type="file"
+            name="files"
+            id="files"
+            className="hidden"
+            placeholder="path to folder with svg files"
+            onChange={({ target }) => setFiles(Array.from(target.files || []))}
+          />
+        </div>
+
+        <button
+          onClick={upload}
+          className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        >
+          <UploadIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </button>
+      </div>
+    </label>
+  );
+};
 
 export const NewFile = () => {
   const { query } = useRouter();
@@ -72,8 +179,6 @@ export const NewFile = () => {
 export const Setup: NextPage<SetupProps> = ({ icons }) => {
   const { query } = useRouter();
 
-  const [path, setPath] = useState<string>(query.path as string);
-
   return (
     <div className="container mx-auto p-2">
       <AuthBlock />
@@ -90,31 +195,15 @@ export const Setup: NextPage<SetupProps> = ({ icons }) => {
           </b>
         </div>
 
-        <div className="my-2 flex flex-row">
-          <input
-            type="string"
-            name="path"
-            id="path"
-            className="block sm:text-sm border-gray-800 border rounded-md rounded-r-none p-2"
-            placeholder="path to folder with svg files"
-            value={path}
-            onChange={({ target }) => setPath(target.value)}
-          />
-          <Link
-            href={`/setup/repository?installation_id=${query.installation_id}&owner=${query.owner}&repository=${query.repository}&path=${path}`}
-          >
-            <a className="flex p-2 border-gray-800 border rounded-md border-l-0 rounded-l-none hover:bg-gray-200">
-              use path
-            </a>
-          </Link>
-        </div>
+        <PathInput />
 
-        <NewFile />
+        <Upload />
 
-        <p>Available icons under /{query.path}:</p>
+        {/* <NewFile /> */}
+
         {!icons ||
           (!icons.length && <div className="mt-2">Nothing found...</div>)}
-        <div className="grid lg:grid-cols-8 md:grid-cols-4 grid-cols-2 gap-1">
+        <div className="mt-4 grid lg:grid-cols-8 md:grid-cols-4 grid-cols-2 gap-1">
           {icons.map(
             (icon) =>
               icon.download_url && (
@@ -126,7 +215,7 @@ export const Setup: NextPage<SetupProps> = ({ icons }) => {
                     rel="noreferrer"
                   >
                     <img
-                      className="w-[24px] h-[24px] mr-4"
+                      className="w-[24px] h-[24px] mb-1"
                       src={icon.download_url}
                     />
                     <p className="font-mono text-xs">{icon.name}</p>
