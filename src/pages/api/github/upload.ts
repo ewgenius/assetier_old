4a812c5a6b3c235ce1e4f665a862572a4083903c
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-12 10:46:40 +0300

    update contents in PR

--- src/pages/api/github/upload.ts
+++ src/pages/api/github/upload.ts
@@ -1,8 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { App } from "@octokit/app";
 import { getSession } from "next-auth/react";
 import { getGitHubPrivateKey } from "@utils/getGitHubPrivateKey";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -29,13 +29,52 @@ export default async function handler(
 
   const body = JSON.parse(req.body);
 
-  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+  // TODO: specify default branch in the UI
+  const branches = await octokit.request("GET /repos/{owner}/{repo}/branches", {
     owner: body.owner,
     repo: body.repository,
-    path: body.path,
-    message: "update from Assetier",
-    content: Buffer.from(body.content).toString("base64"),
   });
+
+  const sha = branches.data[0].commit.sha;
+
+  console.log(sha);
+
+  const branchName = `assetier/${uuidv4()}`;
+  const newBranch = await octokit.request(
+    "POST /repos/{owner}/{repo}/git/refs",
+    {
+      owner: body.owner,
+      repo: body.repository,
+      ref: `refs/heads/${branchName}`,
+      sha,
+    }
+  );
+
+  console.log(newBranch.data);
+
+  const newFile = await octokit.request(
+    "PUT /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner: body.owner,
+      repo: body.repository,
+      path: body.path,
+      message: "update from Assetier",
+      content: Buffer.from(body.content).toString("base64"),
+      branch: branchName,
+    }
+  );
+
+  console.log(newFile.data);
+
+  const pr = await octokit.request("POST /repos/{owner}/{repo}/pulls", {
+    owner: body.owner,
+    repo: body.repository,
+    title: "Assetier update",
+    base: "main",
+    head: branchName,
+  });
+
+  console.log(pr.data);
 
   res.status(200).json({ name: "John Doe" });
 }
