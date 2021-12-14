197594a93de736a6d1a9b0d957a5d2cd0d1d5013
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-14 22:23:49 +0300

    update darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     extend: {
       sans: ["Inter var", ...defaultTheme.fontFamily.sans],
