581caa9895d973a637f8399f417d92c63347f70b
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-20 11:18:32 +0300

    fix auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPageExtended } from "@utils/types";
 import { Page } from "@components/Page";
 import { PageHeader } from "@components/PageHeader";
 import { LayoutBlock } from "@components/LayoutBlock";
+import { AuthBlock } from "@components/AuthBlock";
 
 export const Home: NextPageExtended = () => {
   return (
     <Page title={() => <PageHeader>Projects</PageHeader>}>
+      <LayoutBlock>
+        <AuthBlock />
+      </LayoutBlock>
       <LayoutBlock>
         <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
       </LayoutBlock>
