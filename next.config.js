9982add2e89e74d9eec10d2237801ab3a5390c32
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-28 00:00:14 +0300

    WIP: subdomain rewrite

--- next.config.js
+++ next.config.js
@@ -5,15 +5,15 @@ module.exports = {
   async rewrites() {
     return {
       beforeFiles: [
+        // {
+        //   source: "/",
+        //   has: [{ type: "host", value: "feather.assetier.app" }],
+        //   destination: "https://www.assetier.app/public/feather",
+        // },
         {
           source: "/",
-          has: [{ type: "host", value: "feather.assetier.app" }],
-          destination: "https://www.assetier.app/public/feather",
-        },
-        {
-          source: "/:path*",
           has: [{ type: "host", value: ":appDomain(.*).assetier.app" }],
-          destination: "https://www.assetier.app/public/:appDomain/:path*",
+          destination: "https://www.assetier.app/public/:appDomain",
         },
       ],
     };
