18a4818856c271aff894e137fbf5e712c50c15a2
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-27 23:28:42 +0300

    WIP: subdomain hosintg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  rewrites: {
+    beforeFiles: [
+      {
+        source: "/:path*",
+        has: { type: "host", value: "(<appDomain>).public.assetier.app" },
+        destination: "/public/:appDomain/:path*",
+      },
+    ],
+  },
 };
