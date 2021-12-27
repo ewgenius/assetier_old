2dfd9ded97c60ccbb30624c4fff31232e1daa60a
Evgenii Khramkov <ewgeniux@gmail.com>
2021-12-28 00:33:08 +0300

    disable rewrites

--- next.config.js
+++ next.config.js
@@ -2,20 +2,20 @@
 module.exports = {
   reactStrictMode: true,
 
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // {
-        //   source: "/",
-        //   has: [{ type: "host", value: "feather.assetier.app" }],
-        //   destination: "https://www.assetier.app/public/feather",
-        // },
-        {
-          source: "/",
-          has: [{ type: "host", value: "(.*).assetier.app" }],
-          destination: "https://www.assetier.app/public/$1",
-        },
-      ],
-    };
-  },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       // {
+  //       //   source: "/",
+  //       //   has: [{ type: "host", value: "feather.assetier.app" }],
+  //       //   destination: "https://www.assetier.app/public/feather",
+  //       // },
+  //       // {
+  //       //   source: "/",
+  //       //   has: [{ type: "host", value: "(?!.*www).assetier.app" }],
+  //       //   destination: "https://www.assetier.app/public/$1",
+  //       // },
+  //     ],
+  //   };
+  // },
 };
