13aacc1ec5a6edc80b5348597c57d2049fe9451e
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-02 00:32:59 +0300

    reuse component

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -15,7 +15,7 @@ export const AssetCard: FC<AssetCardProps> = ({ asset, onClick }) => {
       >
         <div className="h-28 w-full p-2 overflow-hidden flex flex-col flex-grow justify-center items-center rounded-md bg-white group-hover:outline outline-offset-2 outline-2 outline-zinc-500">
           <img
-            className="w-[32px] h-[32px]"
+            className="w-[28px] h-[28px]"
             src={asset.download_url}
             alt={asset.name}
           />
--- src/components/AssetsGrid.tsx
+++ src/components/AssetsGrid.tsx
@@ -1,39 +1,13 @@
 import type { GithubFile } from "@utils/types";
 import type { FC } from "react";
-import { LayoutBlock } from "./LayoutBlock";
-import { Spinner } from "./Spinner";
+import { AssetCard } from "@components/AssetCard";
+import { LayoutBlock } from "@components/LayoutBlock";
+import { Spinner } from "@components/Spinner";
 
 export interface AssetsGridProps {
   assets?: GithubFile[];
   onClick?: (asset: GithubFile) => void;
 }
-
-export interface AssetCardProps {
-  asset: GithubFile;
-  onClick?: (asset: GithubFile) => void;
-}
-
-export const AssetCard: FC<AssetCardProps> = ({ asset, onClick }) => {
-  return (
-    <div className="flex">
-      <button
-        className="max-w-full flex flex-col flex-grow justify-center items-center group"
-        onClick={() => onClick && onClick(asset)}
-      >
-        <div className="h-28 w-full p-2 overflow-hidden flex flex-col flex-grow justify-center items-center rounded-md bg-white group-hover:outline outline-offset-2 outline-2 outline-zinc-500">
-          <img
-            className="w-[32px] h-[32px]"
-            src={asset.download_url}
-            alt={asset.name}
-          />
-        </div>
-        <div className="p-2 overflow-hidden max-w-full">
-          <p className="font-mono text-[10px] truncate">{asset.name}</p>
-        </div>
-      </button>
-    </div>
-  );
-};
 
 export const AssetsGrid: FC<AssetsGridProps> = ({ assets, onClick }) => {
   return (
