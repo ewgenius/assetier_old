aa49d4ea65fa1a3ecfd2af4c381adca0291b2c2d
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-02 23:32:51 +0300

    mobile org switcher

--- src/components/AppTopBar.tsx
+++ src/components/AppTopBar.tsx
@@ -28,11 +28,11 @@ export interface AppTopBarProps {
   currentNavId: string;
 }
 
-interface ProfileDropdownProps {
+interface OrganizationDropdownProps {
   onCreateOrganizationClick: () => void;
 }
 
-const ProfileDropdown: FC<ProfileDropdownProps> = ({
+const OrganizationDropdown: FC<OrganizationDropdownProps> = ({
   onCreateOrganizationClick,
 }) => {
   const { user } = useMe();
@@ -44,11 +44,11 @@ const ProfileDropdown: FC<ProfileDropdownProps> = ({
   }
 
   return (
-    <Menu as="div" className="ml-3 relative">
-      <div>
-        <Menu.Button className="max-w-xs flex items-center text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500">
-          <div className="ml-2 mr-4 flex">
-            <div className="font-medium text-sm text-gray-800 mr-1">
+    <Menu as="div" className="mx-2 relative sm:w-auto">
+      <div className="flex">
+        <Menu.Button className="sm:max-w-xs flex flex-grow items-center text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500">
+          <div className="flex flex-grow mx-2">
+            <div className="font-medium text-lg sm:text-sm text-gray-800 mr-1">
               {currentOrganization.type === OrganizationType.PERSONAL
                 ? user.user.name
                 : currentOrganization.name}
@@ -59,14 +59,7 @@ const ProfileDropdown: FC<ProfileDropdownProps> = ({
               </div>
             </div>
           </div>
-          <span className="sr-only">Open user menu</span>
-          {user.user.image && (
-            <img
-              className="h-8 w-8 rounded-full"
-              src={user.user.image}
-              alt=""
-            />
-          )}
+          <span className="sr-only">Open organization menu</span>
         </Menu.Button>
       </div>
       <Transition
@@ -78,7 +71,7 @@ const ProfileDropdown: FC<ProfileDropdownProps> = ({
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-full sm:w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <Menu.Item key={user.personalOrganization.id}>
             {({ active }) => (
               <button
@@ -141,9 +134,43 @@ const ProfileDropdown: FC<ProfileDropdownProps> = ({
               <PlusCircleIcon className="w-4 h-4" />
             </button>
           </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
 
-          <div className="border-b border-gray-200 my-1 mx-2" />
+const ProfileDropdown: FC = () => {
+  const { user } = useMe();
 
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <Menu as="div" className="ml-3 relative">
+      <div>
+        <Menu.Button className="max-w-xs flex items-center text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500">
+          <span className="sr-only">Open user menu</span>
+          {user.user.image && (
+            <img
+              className="h-8 w-8 rounded-full"
+              src={user.user.image}
+              alt=""
+            />
+          )}
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           {userNavigation.map((item) => (
             <Menu.Item key={item.name}>
               {({ active }) => (
@@ -188,9 +215,11 @@ export const AppTopBar: FC<AppTopBarProps> = () => {
                   </div>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                  <ProfileDropdown
+                  <OrganizationDropdown
                     onCreateOrganizationClick={() => setOpen(true)}
                   />
+
+                  <ProfileDropdown />
                 </div>
                 <div className="-mr-2 flex items-center sm:hidden">
                   {/* Mobile menu button */}
@@ -208,7 +237,11 @@ export const AppTopBar: FC<AppTopBarProps> = () => {
 
             <Disclosure.Panel className="sm:hidden">
               <div className="pt-4 pb-3 border-t border-gray-200">
-                <div className="flex items-center px-4">
+                <OrganizationDropdown
+                  onCreateOrganizationClick={() => setOpen(true)}
+                />
+
+                {/* <div className="flex items-center px-4">
                   <div className="flex-shrink-0">
                     {user.user.image && (
                       <img
@@ -228,7 +261,8 @@ export const AppTopBar: FC<AppTopBarProps> = () => {
                       </div>
                     </div>
                   </div>
-                </div>
+                </div> */}
+
                 <div className="mt-3 space-y-1">
                   {userNavigation.map((item) => (
                     <Disclosure.Button
