021705f9e7a1258857350bb36337898aa72e2e10
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-06 22:39:36 +0300

    WIP: get figma file contents api

--- src/pages/app/[organizationId]/projects/[projectId]/index.tsx
+++ src/pages/app/[organizationId]/projects/[projectId]/index.tsx
@@ -13,69 +13,72 @@ import { useProjectBranches } from "@hooks/useProjectBranches";
 import { useDelayedInputState } from "@hooks/useInputState";
 import { AssetsToolbar } from "@components/AssetsToolbar";
 
-export const ProjectPage: NextPageExtended<{}, {}, ProjectPageWrapperProps> =
-  () => {
-    const project = useProjectContext();
-    const [uploadOpen, setUploadOpen] = useState(false);
-    const { branches } = useProjectBranches(project);
-    const [selectedBranch, setSelectedBranch] = useState<GithubBranch | null>(
-      null
-    );
-    const { contents } = useProjectContents(
-      project.id,
-      selectedBranch?.name || project.defaultBranch
-    );
-    const [selectedAsset, setSelectedAsset] = useState<GithubFile | null>(null);
-    const [query, delayedQuery, setQuery, delaying] = useDelayedInputState();
+export const ProjectPage: NextPageExtended<
+  {},
+  {},
+  ProjectPageWrapperProps
+> = () => {
+  const project = useProjectContext();
+  const [uploadOpen, setUploadOpen] = useState(false);
+  const { branches } = useProjectBranches(project);
+  const [selectedBranch, setSelectedBranch] = useState<GithubBranch | null>(
+    null
+  );
+  const { contents } = useProjectContents(
+    project.id,
+    selectedBranch?.name || project.defaultBranch
+  );
+  const [selectedAsset, setSelectedAsset] = useState<GithubFile | null>(null);
+  const [query, delayedQuery, setQuery, delaying] = useDelayedInputState();
 
-    // useEffect(() => {
-    //   if (
-    //     selectedBranch &&
-    //     branches &&
-    //     project.defaultBranch !== selectedBranch.name
-    //   ) {
-    //     const newBranch = branches.find(
-    //       (b) => b.name === project.defaultBranch
-    //     );
-    //     if (newBranch) {
-    //       setSelectedBranch(newBranch);
-    //     }
-    //   }
-    // }, [project.defaultBranch, branches, selectedBranch]);
+  // useEffect(() => {
+  //   if (
+  //     selectedBranch &&
+  //     branches &&
+  //     project.defaultBranch !== selectedBranch.name
+  //   ) {
+  //     const newBranch = branches.find(
+  //       (b) => b.name === project.defaultBranch
+  //     );
+  //     if (newBranch) {
+  //       setSelectedBranch(newBranch);
+  //     }
+  //   }
+  // }, [project.defaultBranch, branches, selectedBranch]);
 
-    return (
-      <>
-        <AssetsToolbar
-          project={project}
-          query={query}
-          onQueryChange={setQuery}
-          querying={delaying}
-          onUploadClick={() => setUploadOpen(true)}
-          branches={branches}
-          selectedBranch={selectedBranch}
-          onSelectBranch={setSelectedBranch}
-        />
-        <AssetsGrid
-          assets={contents?.filter(
-            (asset) => asset.download_url && asset.name.includes(delayedQuery)
-          )}
-          onClick={setSelectedAsset}
-        />
-        <UploadSlideOver
-          project={project}
-          baseBranch={selectedBranch}
-          open={uploadOpen}
-          onClose={() => setUploadOpen(false)}
-        />
-        <AssetDetailsSlideOver
-          project={project}
-          asset={selectedAsset}
-          open={!!selectedAsset}
-          onClose={() => setSelectedAsset(null)}
-        />
-      </>
-    );
-  };
+  return (
+    <>
+      <AssetsToolbar
+        project={project}
+        query={query}
+        onQueryChange={setQuery}
+        querying={delaying}
+        onUploadClick={() => setUploadOpen(true)}
+        branches={branches}
+        selectedBranch={selectedBranch}
+        onSelectBranch={setSelectedBranch}
+      />
+      <AssetsGrid
+        assets={contents?.filter(
+          (asset) => asset.download_url && asset.name.includes(delayedQuery)
+        )}
+        onClick={setSelectedAsset}
+      />
+      <UploadSlideOver
+        project={project}
+        baseBranch={selectedBranch}
+        open={uploadOpen}
+        onClose={() => setUploadOpen(false)}
+      />
+      <AssetDetailsSlideOver
+        project={project}
+        asset={selectedAsset}
+        open={!!selectedAsset}
+        onClose={() => setSelectedAsset(null)}
+      />
+    </>
+  );
+};
 
 ProjectPage.type = "app";
 ProjectPage.navId = "project";
