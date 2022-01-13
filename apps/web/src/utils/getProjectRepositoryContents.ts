814ac5b6d970814ea087b987303ad08f2911abd8
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-13 10:17:44 +0300

    use sha in asset url

--- apps/web/src/utils/getProjectRepositoryContents.ts
+++ apps/web/src/utils/getProjectRepositoryContents.ts
@@ -6,11 +6,13 @@ import { getProjectRepository } from "@utils/getProjectRepository";
 
 export async function getProjectRepositoryContents(
   project: Project,
-  branch?: string
+  branchName?: string
 ) {
   const installation = await getProjectInstallation(project);
   const octokit = await getOctokit(installation.installationId);
   const repository = await getProjectRepository(project, octokit);
+
+  const currentBranchName = branchName || project.defaultBranch;
 
   const contents = await octokit.request(
     "GET /repos/{owner}/{repo}/contents/{path}",
@@ -18,13 +20,28 @@ export async function getProjectRepositoryContents(
       owner: repository.owner.login as string,
       repo: repository.name as string,
       path: project.assetsPath,
-      ref: `${branch || project.defaultBranch}`,
+      ref: currentBranchName,
     }
   );
 
-  const assets = (contents.data as GithubFile[]).filter((f) =>
-    f.name.endsWith(".svg")
+  const branch = await octokit.request(
+    "GET /repos/{owner}/{repo}/git/ref/{ref}",
+    {
+      owner: repository.owner.login as string,
+      repo: repository.name as string,
+      ref: `heads/${currentBranchName}`,
+    }
   );
+
+  const assets = (contents.data as GithubFile[])
+    .filter((f) => f.name.endsWith(".svg"))
+    .map((asset) => ({
+      ...asset,
+      download_url: asset.download_url.replace(
+        currentBranchName,
+        branch.data.object.sha
+      ),
+    }));
 
   return assets;
 }
