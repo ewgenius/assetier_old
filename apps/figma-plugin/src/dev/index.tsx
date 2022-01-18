a9a0fffa25692e648ca75f526bd903fe0f3cc915
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-18 21:38:23 +0300

    update dev page

--- apps/figma-plugin/src/dev/index.tsx
+++ apps/figma-plugin/src/dev/index.tsx
@@ -1,23 +1,46 @@
-import React from "react";
-import { FC, useRef, useCallback } from "react";
+import React, {
+  PropsWithChildren,
+  FC,
+  useRef,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { render } from "react-dom";
 import { MessageType, PluginMessage } from "../types";
 
-// // @ts-ignore
-// if (module.hot) {
-//   // @ts-ignore
-//   module.hot.accept();
-// }
+export const Button: FC<PropsWithChildren<{ onClick: () => void }>> = ({
+  children,
+  onClick,
+}) => (
+  <button
+    type="button"
+    className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-gray-700 hover:bg-gray-800"
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+const usePersistedState = (defaultValue: string) => {};
 
 export const DebugShell: FC = () => {
   const frameRef = useRef<HTMLIFrameElement>(null);
 
-  React.useEffect(() => {
-    const frame = frameRef.current;
-    if (frame) {
-      console.log(frame);
-    }
-  }, [frameRef]);
+  const [token, setToken] = useState(
+    localStorage.getItem("assetier.token") || ""
+  );
+  const [organizationId, setOrganizationId] = useState(
+    localStorage.getItem("assetier.organizationId") || ""
+  );
+  const [projectId, setProjectId] = useState(
+    localStorage.getItem("assetier.projectId") || ""
+  );
+
+  const setTokenHandler = (value: string) => {
+    setToken(value);
+    localStorage.setItem("key", value);
+  };
 
   const postMessage = useCallback(
     (pluginMessage: PluginMessage) => {
@@ -33,10 +56,20 @@ export const DebugShell: FC = () => {
     <div className="h-full w-full flex">
       <div className="bg-gray-900 text-gray-200 h-full flex flex-col min-w-[320px]">
         <div className="p-4 border-b border-gray-700">Plugin controls</div>
+
         <div className="flex-grow p-4 flex flex-col gap-2 max-w-sm">
-          <button
-            type="button"
-            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-gray-700 hover:bg-gray-800"
+          <Button
+            onClick={() =>
+              postMessage({
+                type: MessageType.Init,
+                data: {},
+              })
+            }
+          >
+            emulate: plugin started (no user data)
+          </Button>
+
+          <Button
             onClick={() =>
               postMessage({
                 type: MessageType.Init,
@@ -47,12 +80,28 @@ export const DebugShell: FC = () => {
               })
             }
           >
-            emulate: plugin started
-          </button>
+            emulate: plugin started (only user token)
+          </Button>
 
-          <button
-            type="button"
-            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-gray-700 hover:bg-gray-800"
+          <Button
+            onClick={() =>
+              postMessage({
+                type: MessageType.Init,
+                data: {
+                  token:
+                    "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..GZBEKBJpKiyRBkoN.aM9MUdwcxQ8QKYcrwPTdtchQLMKZZwYrdO7EGkpcc1VE4ZjLW731GBjkkRE9ekX07hRRb2zOLTTiqw0wk6cIzv7Z4z8nlGj4DgJqq9LQKOk7XoObVXLz1-V05Ipjw1Mln7c0iRht_FGFunRHSsfW-ifKZqiywHmUuTtIp_LlondXJVZ3BsbdavtM4ZX835SesoOIvDdtGUcMQJdY40eDPgbM5i0gOmKBgvq7lNJT50HRkczE3YJRY-qbr6CMdkS-Kwmy2QRNVleRy0-rXJMtSUXz2frO70jBmA9JgeETIFIYVBvUBSDiZtwSugeXIBPoKmqsnytNbZNXjN3zabFlrkeoTTvnVCV1xhq4fUk.5VyILaOLGr4AiDwRRraBBA",
+                  organizationId: "ckxxi36d30541ujndn755fii3",
+                  projectId: "cky0ly54k0072jmnd5q3fmqrl",
+                },
+              })
+            }
+          >
+            emulate: plugin started (user token and org/project)
+          </Button>
+
+          <div className="border-b border-gray-700 my-2" />
+
+          <Button
             onClick={() =>
               postMessage({
                 type: MessageType.ReceiveSelectedNodes,
@@ -66,7 +115,7 @@ export const DebugShell: FC = () => {
             }
           >
             emulate: figma nodes selected
-          </button>
+          </Button>
         </div>
       </div>
 
