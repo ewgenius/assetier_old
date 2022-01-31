2de70ff99b1c24198036ef5efa6cc9655e365d1d
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-31 18:19:34 +0300

    fix

--- apps/web/src/utils/auth0JwtMiddleware.ts
+++ apps/web/src/utils/auth0JwtMiddleware.ts
@@ -16,7 +16,7 @@ export const auth0JwtMiddleware: Middleware = (
         jwksUri: `${process.env.AUTH0_ISSUER_BASE_URL}/.well-known/jwks.json`,
       }),
       audience: process.env.AUTH0_API_AUDIENCE,
-      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+      issuer: `${process.env.AUTH0_ISSUER_BASE_URL}/`,
       algorithms: ["RS256"],
     })(req as any, res as any, (err) => {
       if (err) {
