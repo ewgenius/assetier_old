ac8396555201055a46940d8b098f0ed201e054d5
Evgenii Khramkov <ewgeniux@gmail.com>
2022-01-31 10:10:34 +0300

    fix type

--- apps/figma-plugin/src/app/hooks/useMe.ts
+++ apps/figma-plugin/src/app/hooks/useMe.ts
@@ -6,8 +6,8 @@ import type { UserMe } from "@assetier/types";
 export function useMe() {
   const { token } = useAppContext();
   const { data, error } = useSWR<UserMe>(
-    `${process.env.API_URL}/api/figma/auth/me`,
-    authFetcher(token)
+    `${process.env.API_URL}/api/figma/plugin/me`,
+    authFetcher(token as string)
   );
   return {
     user: data,
