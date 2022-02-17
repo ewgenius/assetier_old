9790043aaa9aa5eb2ec43e1b840d0e941b56415a
Evgenii Khramkov <ewgeniux@gmail.com>
2022-02-17 22:24:03 +0300

    cleanup styles

--- apps/figma-plugin/src/app/pages/MainPage.tsx
+++ apps/figma-plugin/src/app/pages/MainPage.tsx
@@ -71,7 +71,7 @@ export const MainPage: FC = () => {
     <Page>
       <ProfileTopbar />
 
-      <div className="flex flex-grow flex-col gap-2 overflow-y-auto p-4">
+      <div className="flex flex-grow flex-col gap-2 overflow-y-auto p-2">
         {/* <button
           type="button"
           className="focus:ring-zink-500 flex w-full items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
@@ -81,46 +81,6 @@ export const MainPage: FC = () => {
         </button> */}
         {selectedNodes && selectedNodes.length > 0 ? (
           <>
-            <div className="flex items-center gap-2">
-              <button
-                type="button"
-                disabled={exporting}
-                className="focus:ring-zink-500 flex w-full items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                onClick={exportNodes}
-              >
-                <span>Export selected nodes</span>
-                <span className="flex-grow" />
-                {exporting && <Spinner className="ml-1" size={2} />}
-              </button>
-
-              <button
-                type="button"
-                disabled={exporting}
-                className="focus:ring-zink-500 flex  items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                onClick={exportNodes}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              </button>
-            </div>
             {/* 
             <div className="relative flex items-start">
               <div className="flex h-5 items-center">
@@ -170,8 +130,50 @@ export const MainPage: FC = () => {
               )}
           </>
         ) : (
-          <p className="text-xs text-gray-600">Select some elements first</p>
+          <p className="p-2 text-xs text-gray-400">
+            Select some elements first...
+          </p>
         )}
+      </div>
+
+      <div className="flex items-center gap-2 border-t border-gray-200 p-4">
+        <button
+          type="button"
+          disabled={exporting || !(selectedNodes && selectedNodes.length > 0)}
+          className="focus:ring-zink-500 flex flex-grow items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          onClick={exportNodes}
+        >
+          <span>Export selected nodes</span>
+          <span className="flex-grow" />
+          {exporting && <Spinner className="ml-1" size={2} />}
+        </button>
+
+        <button
+          type="button"
+          disabled={exporting}
+          className="focus:ring-zink-500 flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </button>
       </div>
     </Page>
   );
